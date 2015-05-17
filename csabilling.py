d021fd65ed9a749c3811ae14ab35717e6e42d3c6
ahhda <highlightanuj@gmail.com>
2015-05-17 22:06:27 +0530

    Script

--- csabilling.py
+++ csabilling.py
@@ -7,7 +7,7 @@ import xlrd
 import xlwt
 
 driver = webdriver.Firefox()
-driver.get("http://www.bits-pilani.ac.in:12349/Login.aspx")
+driver.get("your site")
 #assert "Python" in driver.title
 xpaths = { 'user' : "//input[@id='TextBox1']",
            'pass' : "//input[@id='TextBox2']",
@@ -22,12 +22,11 @@ xpaths = { 'user' : "//input[@id='TextBox1']",
            'addnext' : "//input[@id='Wizard1_StepNavigationTemplateContainerID_StepNextButton']",
            'finish' : "//input[@id='Wizard1_FinishNavigationTemplateContainerID_FinishButton']",
          }
-username = "a7"
-password = "a72011"
+username = ""
+password = ""
 billnam = "TSHIRT_2015"
-#test = "2013A7PS100"
 amount = "220"
-book = xlrd.open_workbook('C:\Users\Anuj\Desktop\csatshirt.xlsx')
+book = xlrd.open_workbook('your excel sheet')
 first_sheet = book.sheet_by_index(0)
 driver.find_element_by_xpath(xpaths['user']).clear()
 
@@ -49,19 +48,9 @@ driver.find_element_by_xpath(xpaths['billnext']).click()
 for i in range(174,221):
     cell = first_sheet.cell(i,0)
     test = str(cell.value)
-    if test[-1] == "P":
-        test = test[:-1]
     driver.find_element_by_xpath(xpaths['id']).clear()
     driver.find_element_by_xpath(xpaths['id']).send_keys(test)
     driver.find_element_by_xpath(xpaths['amount']).clear()
     driver.find_element_by_xpath(xpaths['amount']).send_keys(amount)
     driver.find_element_by_xpath(xpaths['add']).click()
 
-
-#driver.find_element_by_xpath(xpaths['addnext']).click()
-#driver.find_element_by_xpath(xpaths['finish']).click()
-"""elem = driver.find_element_by_name("q")
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()"""
